31f5061ee159f2b3bfb3d0c08985b8f925bf69ee
aurelien.pisu <aurelien.pisu@gmail.com>
2023-12-12 22:00:30 +0100

    start refactoring micro service

--- ycappuccino/endpoints/beans.py
+++ ycappuccino/endpoints/beans.py
@@ -106,3 +106,77 @@ class UrlPath(object):
 
     def get_params(self):
         return self._query_param
+
+class EndpointResponseModel(EndpointResponse):
+
+    def __init__(self, status,a_header=None, a_meta=None, a_body=None, a_storage_property="_mongo_model"):
+        """ need status"""
+        super(EndpointResponseModel,self).__init__(status,a_header,a_meta, a_body)
+        self._storage_property = a_storage_property
+
+    def get_json(self):
+        if self._meta is None:
+            return json.dumps(self._body)
+        else:
+            w_resp = {
+                "status": self._status,
+                "meta": self._meta,
+                "data": None
+            }
+            if self._body is not None:
+                if isinstance(self._body, dict):
+                    w_resp["data"] = self._body
+                if self._storage_property in self._body.keys():
+                    w_resp["data"]  = self._body[self._storage_property]
+                elif isinstance(self._body, list) :
+                    w_body = []
+                    if len(self._body) > 0 :
+                        if self._storage_property in self._body[0].keys():
+                            for w_model in self._body:
+                                w_body.append(w_model[self._storage_property])
+                        else:
+                            for w_json in self._body:
+                                w_body.append(w_json)
+                    w_resp["data"] = w_body
+            else:
+                if w_resp["meta"]["type"] == "array":
+                    w_resp["data"] = []
+                else:
+                    w_resp["data"] = {}
+
+            return json.dumps(w_resp)
+
+
+class UrlPathModel(UrlPath):
+
+    def __init__(self, a_method, a_url, a_api_description):
+        """ need status"""
+        super().__init__(a_method, a_url, a_api_description)
+
+        self._is_schema = "$schema" in self.get_split_url()
+        self._is_multipart = "$multipart" in self.get_split_url()
+
+        self._is_empty = "$empty" in self.get_split_url()
+
+
+
+    def is_crud(self):
+        return  not self._is_schema and not self._is_empty and not self._is_multipart
+
+    def is_draft(self):
+        return  self._query_param is not None and "draft" in self._query_param
+
+    def get_draft(self):
+        return self._query_param["draft"] if self.is_draft() else None
+
+    def is_schema(self):
+        return self._is_schema
+
+    def is_multipart(self):
+        return self._is_multipart
+
+    def is_empty(self):
+        return self._is_empty
+
+    def get_item_plural_id(self):
+        return self._item_plural_id
